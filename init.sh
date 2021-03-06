bbad600982254d7e2764b3418fe312290f93899a
wind <mofeng64@gmail.com>
2021-03-06 11:45:27 +0800

    先测试一下

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+if type lftp >/dev/null 2>&1; then 
+  echo 'exists lftp' 
+else 
+  echo 'no exists lftp'
+  sudo apt-get install lftp 
+fi
+echo "=== Environment variables ==="
+echo "INPUT_SERVER: ${INPUT_SERVER}"
+echo $INPUT_SERVER
