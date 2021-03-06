bcb6793cab0e470b813a117914a7366e2233c3d1
wind <mofeng64@gmail.com>
2021-03-06 12:00:11 +0800

    添加内容

--- init.sh
+++ init.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "=== Environment variables ==="
+echo "INPUT_SERVER: ${INPUT_SERVER}"
+echo $INPUT_SERVER
 
+if type lftp >/dev/null 2>&1; then 
+  echo 'exists lftp' 
+else 
+  echo 'no exists lftp'
+  sudo apt-get install lftp 
+fi
