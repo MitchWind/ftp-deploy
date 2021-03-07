16de04769367d3f3c055862b98d6a906b6a31495
wind <mofeng64@gmail.com>
2021-03-07 17:45:20 +0800

    测试一下上传

--- init.sh
+++ init.sh
@@ -1,13 +1,42 @@
 #!/bin/bash
-pwd
-echo "=== Environment variables ==="
-echo $INPUT_SERVER
+FTP_SETTINGS="set ftp:ssl-allow ${INPUT_SSL_ALLOW};"
+FTP_SETTINGS="${FTP_SETTINGS} set net:timeout ${INPUT_NET_TIMEOUT};"
+FTP_SETTINGS="${FTP_SETTINGS} set net:max-retries ${INPUT_NET_MAX_RETRIES};"
 
-echo "测试1：${INPUT_SERVER}"
+MIRROR_COMMAND="mirror --continue --reverse"
+
+if [ -z "${INPUT_SETTINGS}" ]; then
+  FTP_SETTINGS="${FTP_SETTINGS} ${INPUT_SETTINGS}"
+fi
+
+if [ -z "${INPUT_SERVER_DIR}" ]; then
+  INPUT_SERVER_DIR="./"
+else
+  if [ "${INPUT_SERVER_DIR}" != "./" ]; then
+    INPUT_SERVER_DIR="${INPUT_SERVER_DIR}/"
+  fi
+fi
+
+if [ -z "${INPUT_LOCAL_DIR}" ]; then
+  INPUT_LOCAL_DIR="./"
+else
+  if [ "${INPUT_LOCAL_DIR}" != "./" ]; then
+    INPUT_LOCAL_DIR="${INPUT_LOCAL_DIR}/"
+  fi
+fi
+
+if [ "${INPUT_DELETE}" = "true" ]; then
+  MIRROR_COMMAND="${MIRROR_COMMAND} --delete"
+fi
 
 if type lftp >/dev/null 2>&1; then 
-  echo 'exists lftp' 
+  echo 'exists lftp'
 else 
-  echo 'no exists lftp'
   sudo apt-get install lftp 
 fi
+
+lftp \
+  --debug \
+  -u "${INPUT_USERNAME}","${INPUT_PASSWORD}" \
+  "${INPUT_SERVER}" \
+  -e "${FTP_SETTINGS} ${MIRROR_COMMAND} ${INPUT_LOCAL_DIR} ${INPUT_SERVER_DIR}; quit;"
