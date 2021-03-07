b4c8e35a4d599af64e430df0dc5f9492bf894da0
wind <mofeng64@gmail.com>
2021-03-07 18:52:16 +0800

    乱顺序的参数

--- init.sh
+++ init.sh
@@ -3,12 +3,12 @@ FTP_SETTINGS="set ftp:ssl-allow ${INPUT_SSL_ALLOW};"
 FTP_SETTINGS="${FTP_SETTINGS} set net:timeout ${INPUT_NET_TIMEOUT};"
 FTP_SETTINGS="${FTP_SETTINGS} set net:max-retries ${INPUT_NET_MAX_RETRIES};"
 
-MIRROR_COMMAND="mirror --continue --reverse --no-perms -v "
-
+MIRROR_COMMAND="mirror --continue --reverse --ascii -v "
+#自定义参数设置
 if [ -z "${INPUT_SETTINGS}" ]; then
   FTP_SETTINGS="${FTP_SETTINGS} ${INPUT_SETTINGS}"
 fi
-
+#服务器路径
 if [ -z "${INPUT_SERVER_DIR}" ]; then
   INPUT_SERVER_DIR="./"
 else
@@ -16,7 +16,7 @@ else
     INPUT_SERVER_DIR="${INPUT_SERVER_DIR}/"
   fi
 fi
-
+#本地文件路径
 if [ -z "${INPUT_LOCAL_DIR}" ]; then
   INPUT_LOCAL_DIR="./"
 else
@@ -24,7 +24,15 @@ else
     INPUT_LOCAL_DIR="${INPUT_LOCAL_DIR}/"
   fi
 fi
-
+#自定义mirror参数
+if [ -z "${INPUT_OPTIONS}" ]; then
+  MIRROR_COMMAND="${MIRROR_COMMAND} ${INPUT_OPTIONS}"
+fi
+#不设置文件权限
+if [ "${INPUT_NO_PERMS}" = "true" ]; then
+  MIRROR_COMMAND="${MIRROR_COMMAND} --no-perms"
+fi
+#删除不存在文件
 if [ "${INPUT_DELETE}" = "true" ]; then
   MIRROR_COMMAND="${MIRROR_COMMAND} --delete"
 fi
